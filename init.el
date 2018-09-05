949ff09f1b1fe184a34e7e903131d7e7540eac2f
Steffen Schober <steffen.schober@posteo.de>
2018-09-05 05:58:46 +0200

    Move org-latex-classes init to user-config and fix void symbol error.

--- init.el
+++ init.el
@@ -268,20 +268,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-  (add-to-list 'org-latex-classes
-               '("beamer"
-                 "\\documentclass\[presentation\]\{beamer\}"
-                 ("\\section\{%s\}" . "\\section*\{%s\}")
-                 ("\\subsection\{%s\}" . "\\subsection*\{%s\}")
-                 ("\\subsubsection\{%s\}" . "\\subsubsection*\{%s\}")))
-  (add-to-list 'org-latex-classes
-               '("koma-article" "\\documentclass{scrartcl}"
-                 ("\\section{%s}" . "\\section*{%s}")
-                 ("\\subsection{%s}" . "\\subsection*{%s}")
-                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-  )
+)
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
@@ -290,6 +277,22 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (with-eval-after-load 'ox-latex
+     (add-to-list 'org-latex-classes
+                 '("koma-article"
+                   "\\documentclass{scrartcl}"
+                   ("\\section{%s}" . "\\section*{%s}")
+                   ("\\subsection{%s}" . "\\subsection*{%s}")
+                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                   ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+     (add-to-list 'org-latex-classes
+                  '("beamer"
+                    "\\documentclass\[presentation\]\{beamer\}"
+                    ("\\section\{%s\}" . "\\section*\{%s\}")
+                    ("\\subsection\{%s\}" . "\\subsection*\{%s\}")
+                    ("\\subsubsection\{%s\}" . "\\subsubsection*\{%s\}")))
+  )
   (require 'ob)
   (require 'ob-shell)
   (require 'ob-ruby)
